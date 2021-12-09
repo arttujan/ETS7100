d6693afb7614a0b480caeabeca1d883166bbf124
JanneKankkunen <janneonkiva@gmail.com>
2021-12-09 13:23:17 +0200

    Splashscreen

--- app/src/main/java/com/example/ets7100/MainActivity.kt
+++ app/src/main/java/com/example/ets7100/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.ets7100
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.ets7100.ui.main.MainFragment
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        supportActionBar?.hide()
+
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
